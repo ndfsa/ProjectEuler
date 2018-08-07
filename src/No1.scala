ad95541e05aeb1bf3c8bd37d93f5efa09b39e60c
ndfsa <sergiomarzana1@gmail.com>
2018-08-07 15:55:02 -0400

    Second commit

--- src/No1.scala
+++ src/No1.scala
@@ -1,3 +1,12 @@
 object No1 {
+  def main(args: Array[String]): Unit = {
+    print(multipleSum(1000))
+  }
 
+  def multipleSum(n: Int): Int = {
+    ((1 to n - 1).toList).foldLeft(0)((acc, x) => {
+      if ((x % 3 == 0) || (x % 5 == 0)) x + acc
+      else acc
+    })
+  }
 }
